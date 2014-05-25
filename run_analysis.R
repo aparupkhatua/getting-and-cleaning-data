137da2283820c85bd87617874e7935d4b248fe8c
aparupkhatua <aparupkhatua@gmail.com>
2014-05-25 14:16:19 +0530

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,58 @@
+
+setwd("F:/")
+data_dir<-"UCI HAR Dataset"
+# dateDownloaded <- date()
+# dateDownloaded
+
+## import labels
+labels <- read.table(paste("data", data_dir, "activity_labels.txt", sep="/"), 
+                     col.names=c("labelcode","label"))
+
+## import features
+features <- read.table(paste("data", data_dir, "features.txt", sep="/"))
+# determine important features
+wanted_feature_indices <- grep("mean\\(|std\\(", features[,2])
+
+## import training set
+training_folder <- paste("data", data_dir, "train", sep="/")
+training_subject <- read.table(paste(training_folder, "subject_train.txt", sep="/"), 
+                               col.names = "subject")
+training_data <- read.table(paste(training_folder, "X_train.txt", sep="/"),
+                            col.names = features[,2], check.names=FALSE)
+training_data <- training_data[,wanted_feature_indices]
+training_labels <- read.table(paste(training_folder, "y_train.txt", sep="/"),
+                              col.names = "labelcode")
+dftraining = cbind(training_labels, training_subject, training_data)
+
+## import test set
+test_folder <- paste("data", data_dir, "test", sep="/")
+test_subject <- read.table(paste(test_folder, "subject_test.txt", sep="/"), 
+                           col.names = "subject")
+test_data <- read.table(paste(test_folder, "X_test.txt", sep="/"),
+                        col.names = features[,2], check.names=FALSE)
+test_data <- test_data[,wanted_feature_indices]
+test_labels <- read.table(paste(test_folder, "y_test.txt", sep="/"),
+                          col.names = "labelcode")
+dftest = cbind(test_labels, test_subject, test_data)
+
+## merge datasets
+df <- rbind(dftraining, dftest)
+
+## replace label codes with the label
+df = merge(labels, df, by.x="labelcode", by.y="labelcode")
+df <- df[,-1]
+
+## reshape the array
+library(reshape2)
+molten <- melt(df, id = c("label", "subject"))
+
+## produce the tidy dataset with mean of each variable 
+## for each activyt and each subject
+tidy <- dcast(molten, label + subject ~ variable, mean)
+
+## write tidy dataset to disk
+write.table(tidy, file="tidy_data.txt", quote=FALSE, row.names=FALSE, sep="\t")
+
+## write codebook to disk
+write.table(paste("* ", names(tidy), sep=""), file="CodeBook.md", quote=FALSE,
+            row.names=FALSE, col.names=FALSE, sep="\t")
